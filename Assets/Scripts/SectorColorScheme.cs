78a7e1bfbe8a66fbd6aca63f969432857b55ab8e
Sam Boyer <sam.boyer276yt@gmail.com>
2020-03-08 14:55:54 +0000

    Create sector-based color scheme

---
+++ Assets/Scripts/SectorColorScheme.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SectorColors", menuName = "ScriptableObjects/SectorColorScheme", order = 1)]
+public class SectorColorScheme : ScriptableObject
+{
+    public string[] categories;
+    public Color[] colors;
+}
