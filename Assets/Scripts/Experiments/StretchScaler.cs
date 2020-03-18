374d72ab6851b857e00411fe0b132756ae44e61e
Sam Boyer <sam.boyer276yt@gmail.com>
2020-03-18 13:48:57 +0000

    Add one-handed position and two-handed rotation controls

--- Assets/Scripts/Experiments/StretchScaler.cs
+++ Assets/Scripts/Experiments/StretchScaler.cs
@@ -18,47 +18,77 @@ public class StretchScaler : MonoBehaviour
 
     float scaleVelocity = 0;
     Vector3 posVelocity = Vector3.zero;
+    Quaternion rotVelocity = Quaternion.identity;
     
-    bool isActive = false;
+    bool wasBothDown = false;
+    bool wasLDown = false;
+    bool wasRDown = false;
+
     float oldDist;
     float startDist = 0;
     public float elasticShaderStretchFactor = 0.5f; //how much to alter the StretchAmount parameter
 
-    Vector3 oldPos;
+    Vector3 oldPos, oldHandVec;
+    Quaternion startRot;
 
     void Update()
     {
-        float dist = (leftHand.position - rightHand.position).magnitude;
-        Vector3 centrePos = (leftHand.position + rightHand.position)/2f; //get centre point of controllers
+        Vector3 lPos = leftHand.position, rPos = rightHand.position;
+        Vector3 handVec = lPos - rPos;
+        float dist = handVec.magnitude;
+        Vector3 centrePos = (lPos + rPos)/2f; //get centre point of controllers
 
-        bool triggersDown = Input.GetAxis("Oculus_CrossPlatform_PrimaryIndexTrigger") > threshold
-                && Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger") > threshold;
+        bool lDown = Input.GetAxis("Oculus_CrossPlatform_PrimaryIndexTrigger") > threshold;
+        bool rDown = Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger") > threshold;
+        bool bothDown = lDown && rDown;
 
-        if(!isActive && triggersDown){
-            isActive = true;
-            oldDist = dist;
-            startDist = dist;
-            oldPos = centrePos;
-            scaleIndicatorLine.gameObject.SetActive(true);
-        }
-        if(isActive){
-            if(!triggersDown){
-                isActive = false;
-                scaleIndicatorLine.gameObject.SetActive(false);
-            }
-            else{
-                scaleVelocity = dist - oldDist;
-                posVelocity = centrePos - oldPos;
-                scaleIndicatorLine.SetPosition(0, leftHand.position);
-                scaleIndicatorLine.SetPosition(1, rightHand.position);
-                scaleIndicatorLine.material.SetFloat("_StretchAmount",(dist - startDist) * elasticShaderStretchFactor+0.5f);
+        if(bothDown){
+            if(!wasBothDown){ //start stretch & rot
+                //isActive = true;
                 oldDist = dist;
-                oldPos = centrePos;
+                startDist = dist;
+
+                oldHandVec = handVec;
+                startRot = transform.rotation;
+                scaleIndicatorLine.gameObject.SetActive(true);
+            }
+            else{ //continue stretch & rot
+                scaleVelocity = dist - oldDist;
+                rotVelocity = Quaternion.FromToRotation(oldHandVec, handVec);
+
+                scaleIndicatorLine.SetPosition(0, lPos);
+                scaleIndicatorLine.SetPosition(1, rPos);
+                scaleIndicatorLine.material.SetFloat("_StretchAmount",(dist - startDist) * elasticShaderStretchFactor+0.5f);
+
+                oldDist = dist;
+                oldHandVec = handVec;
+            }
+        }
+        else{
+            if(wasBothDown){ //end stretch & rot
+                //isActive = false;
+                scaleIndicatorLine.gameObject.SetActive(false);
+                startRot = transform.rotation;
+            }
+            if(lDown){
+                if(!wasLDown || wasRDown){ //start grab on left hand
+                    oldPos = lPos;
+                }
+                posVelocity = lPos - oldPos;
+                oldPos = lPos;
+            }
+            else if(rDown){
+                if(!wasRDown || wasLDown){ //start grab on right hand
+                    oldPos = rPos;
+                }
+                posVelocity = rPos - oldPos;
+                oldPos = rPos;
             }
         }
 
         scaleVelocity *= damping;
         posVelocity *= damping;
+        rotVelocity = Quaternion.Lerp(Quaternion.identity, rotVelocity, damping);
 
         //scale around the controller centre 
         float scaleFactor = 1 + scaleVelocity;
@@ -68,5 +98,13 @@ public class StretchScaler : MonoBehaviour
         //position
         transform.position += posVelocity * moveScale;
 
+        //rotation
+        //Quaternion finalRot = startRot* rotVelocity;
+        Vector3 delta = centrePos-transform.parent.position;
+        transform.position -= delta;
+        transform.parent.position += delta;
+        transform.parent.rotation = rotVelocity * transform.parent.rotation;
+
+        wasBothDown = bothDown; wasLDown = lDown; wasRDown = rDown;
     }
 }
