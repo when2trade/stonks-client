82c9bac5793a1568b84199d571a4e9664cfe276a
Sam Boyer <samboyer@users.noreply.github.com>
2020-03-29 11:40:53 +0100

    Start non-random scatter plotter

---
+++ Assets/Scripts/RelationalScatterPlotter.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Makes a 3D scatter plot from the input dataset.
+/// TODO remove random point/edge data!
+/// </summary>
+public class RelationalScatterPlotter : MonoBehaviour
+{
+    public GameObject pointPrefab, edgePrefab;
+
+    public int pointCount = 100;
+    public Vector3 pointRange = Vector3.one;
+
+    [Range(0,1)]
+    public float edgeThreshold = 0.1f; //how liberally should we make edges?
+
+    //edge colours
+    public Color weakPositiveColor = Color.white, strongPositiveColor = Color.green, 
+        weakNegativeColor = Color.white, strongNegativeColor = Color.red;
+
+    public SectorColorScheme sectorColors;
+    
+    public Transform headTransform; //reference to head for billboarding
+
+    void Start()
+    {
+        MakePlot();
+    }
+
+    /// <summary>
+    /// Make a random spread of points and edges, as children of this object.s
+    /// </summary>
+    void MakePlot(){
+        Vector3[] points = new Vector3[pointCount];
+        for(int i=0;i<pointCount;i++){ //make 'pointCount'x random points in [-0.5,0.5]^3
+            points[i] = new Vector3(
+                Random.Range(-.5f,.5f),
+                Random.Range(-.5f,.5f),
+                Random.Range(-.5f,.5f));
+        }
+
+        List<(int,int)> edges = new List<(int, int)>();
+        for(int i=0;i<pointCount;i++){
+            for(int j=i+1; j<pointCount;j++){ //for each (symmetric!) point pair,
+                if((points[i] - points[j]).magnitude < edgeThreshold){ //if points are close enough, add edge
+                    edges.Add((i,j));
+                }
+            }
+        }
+        Debug.Log(edges.Count+" edges");
+
+        ScaleNegator scaler = GetComponent<ScaleNegator>();
+
+        var p=0;
+        foreach(var symbolCatPair in Dataset.categoryMap){
+            //make and position points
+            GameObject obj = Instantiate(pointPrefab, transform);
+            obj.transform.localPosition = Vector3.Scale(points[p], pointRange);
+
+            obj.GetComponent<PlotPoint>().SetupPoint(headTransform, symbolCatPair.Key);
+
+            //give random sector and company name
+            obj.GetComponent<SpriteRenderer>().color = sectorColors.ColorOfCategory(symbolCatPair.Value);
+
+            scaler.objectsToScale.Add(obj.transform);
+          p+=1;
+          if(p==pointCount) break;
+        }
+
+        
+        foreach((int,int) edge in edges){
+            //make and position edges
+            LineRenderer line = Instantiate(edgePrefab, transform).GetComponent<LineRenderer>();
+            line.SetPositions(new Vector3[]{Vector3.Scale(points[edge.Item1], pointRange),
+                Vector3.Scale(points[edge.Item2], pointRange)});
+
+            if(Random.value > 0.5f) //colour green or red
+                //linear interpolation between weak and strong correlation colours
+                line.startColor = line.endColor = Color.Lerp(weakPositiveColor, strongPositiveColor, Random.value);
+            else
+                line.startColor = line.endColor = Color.Lerp(weakNegativeColor, strongNegativeColor, Random.value);
+        }
+    }
+
+}
--- Assets/Scripts/SectorColorScheme.cs
+++ Assets/Scripts/SectorColorScheme.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System;
 
 [CreateAssetMenu(fileName = "SectorColors", menuName = "ScriptableObjects/SectorColorScheme", order = 1)]
 public class SectorColorScheme : ScriptableObject
 {
     public string[] categories;
     public Color[] colors;
+
+    public Color ColorOfCategory(string cat){
+        return colors[Array.IndexOf(categories, cat)];
+    }
 }
