e317be6bb2d611b004d88168899a0378dd71c769
Sam Boyer <samboyer@users.noreply.github.com>
2020-04-02 22:00:32 +0100

    Plot relational data (finally)

--- Assets/Scripts/RelationalScatterPlotter.cs
+++ Assets/Scripts/RelationalScatterPlotter.cs
@@ -12,10 +12,12 @@ public class RelationalScatterPlotter : MonoBehaviour
     public GameObject pointPrefab, edgePrefab;
 
     public int pointCount = 100;
-    public Vector3 pointRange = Vector3.one;
+    public Vector3 plotScaler = Vector3.one;
 
     [Range(0,1)]
-    public float edgeThreshold = 0.1f; //how liberally should we make edges?
+    public float minCorrelation = 0.1f; //how liberally should we make edges?
+    [Range(0,1)]
+    public float maxCorrelation = 0.1f; 
 
     public SectorColorScheme sectorColors;
     
@@ -30,61 +32,46 @@ public class RelationalScatterPlotter : MonoBehaviour
     /// Make a random spread of points and edges, as children of this object.s
     /// </summary>
     void MakePlot(){
-        Vector3[] points = new Vector3[pointCount];
-        for(int i=0;i<pointCount;i++){ //make 'pointCount'x random points in [-0.5,0.5]^3
-            points[i] = new Vector3(
-                Random.Range(-.5f,.5f),
-                Random.Range(-.5f,.5f),
-                Random.Range(-.5f,.5f));
-        }
-
-        List<(int,int)> edges = new List<(int, int)>();
-        for(int i=0;i<pointCount;i++){
-            for(int j=i+1; j<pointCount;j++){ //for each (symmetric!) point pair,
-                if((points[i] - points[j]).magnitude < edgeThreshold){ //if points are close enough, add edge
-                    edges.Add((i,j));
-                }
-            }
-        }
-        Debug.Log(edges.Count+" edges");
-
         ScaleNegator scaler = GetComponent<ScaleNegator>();
+        int totalEdges=0;
+        
+        foreach(var symbolIndexPair in Dataset.indexMap){
+            string symbol = symbolIndexPair.Key;
+            int index = symbolIndexPair.Value;
+            Vector3 p1 = Vector3.Scale(Dataset.points[index], plotScaler);
 
-        var syms = new List<string>();
-
-        int p=0;
-        foreach(var symbolCatPair in Dataset.categoryMap){
-            //make and position points
             GameObject obj = Instantiate(pointPrefab, transform);
-            obj.transform.localPosition = Vector3.Scale(points[p], pointRange);
+            obj.transform.localPosition = p1;
 
-            obj.GetComponent<PlotPoint>().SetupPoint(headTransform, symbolCatPair.Key);
-
-            //give random sector and company name
-            obj.GetComponent<SpriteRenderer>().color = sectorColors.ColorOfCategory(symbolCatPair.Value);
+            obj.GetComponent<PlotPoint>().SetupPoint(headTransform, symbol);
+            obj.GetComponent<SpriteRenderer>().color = sectorColors.ColorOfCategory(Dataset.categoryMap[symbol]);
 
             scaler.objectsToScale.Add(obj.transform);
 
-            syms.Add(symbolCatPair.Key);
-            
-            p+=1;
-            if(p==pointCount) break;
-        }
-        Debug.Log(syms.Count);
-        
-        foreach((int,int) edge in edges){
-            Vector3 p1 = Vector3.Scale(points[edge.Item1], pointRange), 
-              p2 = Vector3.Scale(points[edge.Item2], pointRange);
-            Vector3 vec = (p2-p1);
+            //add relevant edges to plot
+            foreach(var symbolIndexPair2 in Dataset.indexMap){
+                string symbol2 = symbolIndexPair2.Key;
+                int index2 = symbolIndexPair2.Value;
+                if(index2<=index) continue; //skip adding duplicate edges
 
-            //make and position edges
-            Transform obj = Instantiate(edgePrefab, transform).transform;
-            obj.localPosition = (p1+p2)/2.0f;
-            obj.localRotation = Quaternion.LookRotation(vec, Vector3.up);
-            obj.localScale = new Vector3(1,1, vec.magnitude);
+                float relation = Dataset.GetRelation(symbol, symbol2);
 
-            obj.GetComponent<PlotEdge>().SetupEdge(syms[edge.Item1], syms[edge.Item2], Random.value*2f-1f);
+                if(!float.IsNaN(relation) && Mathf.Abs(relation) > minCorrelation && Mathf.Abs(relation) < maxCorrelation){
+                    //instantiate and position edges
+                    Vector3 p2 = Vector3.Scale(Dataset.points[index2], plotScaler);
+                    Vector3 vec = (p2-p1);
+                    Transform edgeObj = Instantiate(edgePrefab, transform).transform;
+                    edgeObj.localPosition = (p1+p2)/2.0f;
+                    edgeObj.localRotation = Quaternion.LookRotation(vec, Vector3.up);
+                    edgeObj.localScale = new Vector3(1,1, vec.magnitude);
+
+                    edgeObj.GetComponent<PlotEdge>().SetupEdge(symbol, symbol2, relation);
+
+                    totalEdges+=1;
+                }
+            }
         }
+        Debug.Log(totalEdges+" total edges");
     }
 
 }
